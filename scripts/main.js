04df87083e0fbb3041a1d5b21d09216dc255f261
Jake Blackmon <9832801@isd535.org>
2021-09-13 09:33:53 -0500

    Changed the square into a ball

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-SVGTextContentElement.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000"
+ctx.arc(240,160,20,0, Math.PI*2, false);
+ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
