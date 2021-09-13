51256a802d0c15e868216781a81e1b6652b53b98
Jake Blackmon <9832801@isd535.org>
2021-09-13 09:23:55 -0500

    Started to Code Breakout

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,8 @@
-// Put your JavaScript here
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+SVGTextContentElement.rect(20, 40, 50, 50);
+ctx.fillStyle = "#FF0000"
+ctx.fill();
+ctx.closePath();
