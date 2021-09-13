d00cbbe6031bbd08332a9bdccd1d839b2b2aa1bd
Jake Blackmon <9832801@isd535.org>
2021-09-13 09:39:27 -0500

    Added Rectangle

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,19 @@ let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
 ctx.beginPath();
+ctx.rect(20,40,50,50);
+ctx.fillStyle = "3FF0000";
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
 ctx.arc(240,160,20,0, Math.PI*2, false);
 ctx.fillStyle = "green";
 ctx.fill();
+ctx.closePath();
+
+ctx.beginPath9;
+ctx.rect(160,10,100,40);
+ctx.stokeStyle = "rgba(0,0,255,0.5)";
+ctx.stroke();
 ctx.closePath();
