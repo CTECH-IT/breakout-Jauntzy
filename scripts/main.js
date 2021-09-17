868816da7cac58e87310b8fe04a1fb5ea23b5003
Jake Blackmon <9832801@isd535.org>
2021-09-17 09:04:08 -0500

    Added mouse movement

--- scripts/main.js
+++ scripts/main.js
@@ -163,8 +163,17 @@ function keyUpHandler(e){
         leftPressed = false;
     }
 }
+
+function mouseMoveHandler(e) {
+    let relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+    }
+}
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+
+document.addEventListener("mousemove", mouseMoveHandler,false);
 
  let interval = setInterval(draw, 10);
 
