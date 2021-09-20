bb1862c5bfcb2e06fd5f28b37cbf364b74a14cca
Jake Blackmon <9832801@isd535.org>
2021-09-20 09:08:51 -0500

    added the code to minus lives

--- scripts/main.js
+++ scripts/main.js
@@ -97,8 +97,11 @@ ctx.clearRect(0,0,canvas.width,canvas.height)
         if(x > paddleX && x < paddleX + paddleWidth) {//paddle check
             dy = -dy;
         } else {//it hit the floor!
-        lives--;
-        alert("GAME OVER");
+            if (life > 0){
+                life--;
+                alert("-1 up");
+            }
+        
         document.location.reload();
         clearInterval(interval); //Needed for browser to end game
         }
