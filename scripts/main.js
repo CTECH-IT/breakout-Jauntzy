de148d70f922ada47553795c6986cd0c6f5bcedd
Jake Blackmon <9832801@isd535.org>
2021-09-20 09:42:15 -0500

    fixed typos

--- scripts/main.js
+++ scripts/main.js
@@ -98,7 +98,7 @@ ctx.clearRect(0,0,canvas.width,canvas.height)
             dy = -dy;
         } else {//it hit the floor!
             if (lives > 0){
-                lives = life-1;
+                lives = lives-1;
                 alert("-1 up");
                 document.location.reload();
                 clearInterval(interval);
